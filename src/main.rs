fa2c8515ada446c5ab5abe80526b605ca10500a3
Harry <harrisonhemstreet@gmail.com>
2022-12-30 15:53:23 -0700

    working setup of actix-web, sqlx, postgres, and obviously rust

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+use serde::{Deserialize, Serialize};
+use sqlx::postgres::PgPoolOptions;
+extern crate dotenv;
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Student {
+    id: i64,
+    name: Option<String>,
+}
+
+#[get("/hello/{name}")]
+async fn greet(name: web::Path<String>) -> impl Responder {
+    format!("Hello {}!", name)
+}
+
+#[get("/get_json")]
+async fn get_json() -> impl Responder {
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect("postgres://root:root@localhost:5440/root")
+        .await
+        .unwrap();
+
+    let student = sqlx::query_as!(Student, "select * from student where id = 1")
+        .fetch_one(&pool)
+        .await
+        .unwrap();
+
+    let serialized = serde_json::to_string(&student).unwrap();
+    format!("serialized: {:?}", serialized);
+    HttpResponse::Ok().body(serialized)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(greet).service(get_json))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
